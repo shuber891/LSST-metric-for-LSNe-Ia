293a4872f277725e35d9e075cb32d54ddbc539d7
Simon Huber <33343757+shuber891@users.noreply.github.com>
2019-11-11 11:29:00 +0100

    Add files via upload
    Updates to create csv document for 91 of 94 observing strategies

--- LSST_cadence_metric.py
+++ LSST_cadence_metric.py
@@ -8,31 +8,15 @@ Created on Tue Aug  6 13:30:24 2019
 
 import numpy as np
 from astropy import units as u
-import matplotlib.pyplot as plt
+import csv
 
 # To run this code you have to download the files from https://me.lsst.eu/gris/
-# and chance the data_path and plot_path files, further you have to specifiy
-# the area in the d_area_observing_strategies dic if area is different from
-# the 18000 deg^2 nominal WFD area
+# and the opsim_runs_strategies_input.csv file from ...
+# Further chance the data_path and output_path files
 
 # This is where your cadence strategy file is located
 data_path = "/afs/mpa/data/shuber/data/LSST_MAF_data/"
-plot_path = "/afs/mpa/home/shuber/research/MicrolensSN/plot/"
-
-# list of observing stratigies to investigate where *.npy file
-# has been stored in your data path
-observing_strategies = ["alt_sched", "alt_sched_rolling", "kraken_2026",
-                        "kraken_2035", "colossus_2667", "pontus_2002",
-                        "pontus_2489", "mothra_2049", "nexus_2097",
-                        "kraken_2044", "kraken_2042", "rolling_10yrs_opsim",
-                        "rolling_mix_10yrs_opsim"]
-observing_strategies = ["alt_sched", "alt_sched_rolling", "kraken_2026",
-                        "colossus_2667", "rolling_10yrs_opsim"]
-
-# This dictionary contains the WFD survey area for cadence strategies.
-# If strategy is not in that dic the nominal value 18000 deg^2 will be assumed
-d_area_observing_strategies = {"kraken_2044": 24700, "pontus_2002": 24700,
-                               "nexus_2097": 24700, "mothra_2049": 24700}
+output_path = "/afs/mpa/home/shuber/research/MicrolensSN/plot/"
 
 
 def f_get_median(array):
@@ -44,71 +28,59 @@ def f_get_median(array):
 
 def f_fit_function(gap_median_filter):
     # has been constructed by fitting 13 observing strategies
-    fit = 2.14 * np.exp(0.36 * gap_median_filter.value)
+    fit = 2.15 * np.exp(0.37 * gap_median_filter.value)
     return fit
 
 
-d_good_delay_from_fit = {}
-for observing_strategy in observing_strategies:
-    if observing_strategy in d_area_observing_strategies.keys():
-        survey_area = d_area_observing_strategies[observing_strategy] * u.deg**2
-    else:
-        survey_area = 18000 * u.deg**2
-    array = np.load("%s%s_SLMetric_new.npy" % (data_path, observing_strategy))
+observing_strategies = []
+file_strategies = open("%sopsim_runs_strategies_input.csv" % data_path, "r")
+filte_to_write_on = open("%sopsim_runs.csv" % output_path, "w")
+writer = csv.writer(filte_to_write_on)
 
-    gap_median_filter = f_get_median(array["gap_median"] * u.day)
-    cumulative_season_length = f_get_median((array["season_length"] * u.day).to(u.year))
+counter_line = 0
+for line in file_strategies:
+    line = line.strip()
+    if counter_line == 0:
+        line_to_write = line.split(",")
+        line_to_write.append("Number of LSNe Ia")
+        writer.writerow(line_to_write)
+    if counter_line >= 1:
+        print line.split(",")
+        observing_strategy = line.split(",")[1][:-3]
 
-    """
-    #Further parameters stored in *.npy but not used
-    area_pixel = array["area"] * u.deg**2
-    healpixId = array["healpixId"]
-    pixRa = array["pixRa"] * u.deg
-    pixDec = array["pixDec"] *u.deg
-    gap_max_filter = array["gap_max"] * u.day
-    gap_median_u =  array["gap_median_u"] * u.day
-    gap_median_g =  array["gap_median_g"] * u.day
-    gap_median_r =  array["gap_median_r"] * u.day
-    gap_median_i =  array["gap_median_i"] * u.day
-    gap_median_z =  array["gap_median_z"] * u.day
-    gap_median_y =  array["gap_median_y"] * u.day
-    """
+        if observing_strategy == "baseline_v1.3_1yrs" or observing_strategy == "tde_illum75_v1.3_10yrs" or observing_strategy == "uer_illum75_v1.3_10yrs":
+            line_to_write = line.split(",")
+            line_to_write.append("-")
+            writer.writerow(line_to_write)
+        else:
+            array = np.load("%s%s_SL.npy" % (data_path, observing_strategy))
 
-    N_OM10 = 45.7
-    survey_area_OM10 = 20000 * u.deg**2
-    cumulative_season_length_OM10 = 2.5 * u.year
+            gap_median_filter = f_get_median(array["gap_median"] * u.day)
+            cumulative_season_length = f_get_median((array["season_length"] * u.day).to(u.year)) 
 
-    # rescale numbers of OM10 with cumulative_season_length and survey_area
-    # of a given cadence strategy
-    total_number_of_LSNeIa_0M10 = N_OM10 * survey_area/survey_area_OM10 * cumulative_season_length/cumulative_season_length_OM10
+            area_pixel = array["area"] * u.deg**2
+            survey_area = np.sum(area_pixel)
 
-    # this estimates the number of well LSNeIa which have a well measured time
-    # delay, i.e. that at least one image as accuracy < 1 percent
-    # and precision < 5 percent
-    number_of_good_delay_LSNeIa = total_number_of_LSNeIa_0M10 / f_fit_function(gap_median_filter)
+            N_OM10 = 45.7
+            survey_area_OM10 = 20000 * u.deg**2
+            cumulative_season_length_OM10 = 2.5 * u.year
 
-    d_good_delay_from_fit[observing_strategy] = number_of_good_delay_LSNeIa
-    # print observing_strategy, survey_area, cumulative_season_length, gap_median_filter, number_of_good_delay_LSNeIa
+            # rescale numbers of OM10 with cumulative_season_length
+            # and survey_area of a given cadence strategy
+            total_number_of_LSNeIa_0M10 = N_OM10 * survey_area/survey_area_OM10 * cumulative_season_length/cumulative_season_length_OM10
 
-# This part creates a bar plot
-observing_strategies_ord = []
-y_follow_up = []
-x_axis = []
-x_ticks = []
-x_counter = 0
-for observing_strategy, total_number in sorted(d_good_delay_from_fit.iteritems(),reverse=True,key=lambda (k,v): (v,k)):  
-    observing_strategies_ord.append(observing_strategy)
+            # this estimates the number of well LSNeIa which have a well
+            # measured time delay, i.e. that at least one image as
+            # accuracy < 1 percent and precision < 5 percent
+            number_of_good_delay_LSNeIa = total_number_of_LSNeIa_0M10 / f_fit_function(gap_median_filter)
 
-    x_counter += 2
-    x_axis.append(x_counter)
-    x_ticks.append(observing_strategy)
-    y_follow_up.append(total_number)
+            print observing_strategy, survey_area, cumulative_season_length, gap_median_filter, number_of_good_delay_LSNeIa
 
-x_axis = np.asarray(x_axis)
-ticks, labels = plt.xticks(x_axis, x_ticks, rotation="vertical")
+            line_to_write = line.split(",")
+            line_to_write.append(number_of_good_delay_LSNeIa)
+            writer.writerow(line_to_write)
 
-plt.bar(x_axis, y_follow_up, color="black", label="LSST + follow-up")
-plt.ylabel("lensed SNe Ia with good delay")
-plt.legend()
-plt.tight_layout()
-plt.savefig("%sMetric.png" % (plot_path), dpi=300)
+    counter_line += 1
+
+file_strategies.close()
+filte_to_write_on.close()
